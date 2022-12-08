6ab0c6e20a85d3adacc06ccd39dec42985224d60
il94 <ilyes.landolsi@outlook.fr>
2022-12-08 19:28:22 +0100

    starting project

---
+++ philo/include/philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilandols <ilandols@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 19:11:22 by ilandols          #+#    #+#             */
+/*   Updated: 2022/12/08 19:15:04 by ilandols         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+int	main(int ac, char **av);
+
+#endif
